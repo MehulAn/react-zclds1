c65edd495aac0ab07243cc30d6c136fbb8677d69
MehulAn <anandmehulszr@gmail.com>
2022-01-14 12:12:07 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
         <h1>Hello StackBlitz!</h1>
         <p>Start editing to see some magic happen :)</p>
       </div>
-      <SampleComponent bgCcolor="voilet" a={18} b={21} />
+      <SampleComponent bgCcolor="voilet" a={10} b={20} />
       <SampleComponent bgColor="blue" a={10} b={20} />
     </>
   );
--- src/SampleComponent.js
+++ src/SampleComponent.js
@@ -1,20 +1,25 @@
-import React from "react";
+import React from 'react';
 
-export function SampleComponent({bgColor}){
+export default function SampleComponent() {
   let styleObj = {
     backgroungColor: bgColor,
   };
-  let a = 10;
-  let b = 20;
-
-  return(
+  let obj = {
+    name: 'mehul',
+  };
+  let arr = [1, 2, 3, 4, 5, 6];
+  let arr2 = [<p>1</p>, <p>2</p>, <p>3</p>, <p>4</p>, <p>5</p>, <p>6</p>];
+  return (
     <>
-    <div style={styleObj}>Heading</div>
-    <div>value of a is : {a}</div>
-    <div>value of b is : {b}</div>
-    <div>
-      sum of {a} and {b} is : {a * b}
+      <div style={styleObj}>Heading</div>
+      <div>value of a is : {a}</div>
+      <div>value of b is : {b}</div>
+      <div>
+        sum of {a} and {b} is : {a * b}
       </div>
-      </>
+      <p>objects are not valid : {obj.name}</p>
+      <p>But arrays are : {arr}</p>
+      <p>But arrays are : {arr2}</p>
+    </>
   );
 }
