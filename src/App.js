497e9c41a35105e899dd1d3c521450abfa9f6fc0
MehulAn <anandmehulszr@gmail.com>
2022-01-15 11:51:58 +0530

    1 file modified, 2 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import './style.css';
 import Home from './Components/Home';
 import AboutUs from './Components/AboutUs';
 import Contact from './Components/Contact';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function App() {
   return (
     <div>
-      <Home />
-      <AboutUs />
-      <Contact />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<AboutUs />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
